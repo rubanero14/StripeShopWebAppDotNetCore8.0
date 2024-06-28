9df765f2d50511a4f9d447782f4461f388500e3b
ruban <JY-RedWood@JY-RUBY>
2024-06-28 16:34:47 +0800

    Added Product Controller and DB Context for Product View

--- StripeShopWebApp/Controllers/ProductController.cs
+++ StripeShopWebApp/Controllers/ProductController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StripeShopWebApp.Data;
 
 namespace StripeShopWebApp.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
+        // Injecting Data into Controller using context constructor
+        private readonly ProductContext _productContext;
+        public ProductController(ProductContext productContext) { 
+            _productContext = productContext;
+        }
+
+        public async Task<IActionResult> Index(string searchMenu)
+        {   
+            var products = await _productContext.Products.ToListAsync();
+
+            if (!string.IsNullOrEmpty(searchMenu)) 
+            {
+                products = await _productContext.Products.Where(c => c.Name.Contains(searchMenu)).ToListAsync();
+                return View(products);
+            }
+            return View(products);
         }
     }
 }
